04f7aa5ad5df4c20ec957de46129e8418c8dd342
Jose <joseajith2007@gmail.com>
2026-09-15 11:24:35 +0530

    Remove synchronous g_Config.Save from GamepadEmu touch event to prevent Android I/O crash

--- UI/GamepadEmu.cpp
+++ UI/GamepadEmu.cpp
@@ -1158,7 +1158,6 @@ GamepadEmuView::GamepadEmuView(const TouchControlConfig &config, float xres, flo
             g_Config.iTouchLayout = (g_Config.iTouchLayout == 1) ? 0 : 1;
             g_Config.touchControlsLandscape.iTouchLayout = g_Config.iTouchLayout;
             g_Config.touchControlsPortrait.iTouchLayout = g_Config.iTouchLayout;
-            g_Config.Save("GamepadEmu::SwitchLayout");
             }
         });
     }
